6aa1ac3c1ef3e9828055ecdc597de304c579da47
Mayank1203 <mayankpatle1203@gmail.com>
2026-04-12 11:28:52 +0530

    chore(deps): initialise Spring Boot 3.3 project with all dependencies in pom.xml

---
+++ devsense-backend/src/main/java/com/devsense/DevsenseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsenseBackendApplication.class, args);
+	}
+
+}
---
+++ devsense-backend/src/test/java/com/devsense/DevsenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsenseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
